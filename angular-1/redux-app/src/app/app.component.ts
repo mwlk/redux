a00769009f6c5f1412f946255c624c2dbb3741f5
Mirko Wlk <mirkoivowlk@gmail.com>
2022-11-24 00:47:45 -0300

    call store

--- angular-1/redux-app/src/app/app.component.ts
+++ angular-1/redux-app/src/app/app.component.ts
@@ -1,27 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './redux/counter.actions';
+interface AppState {
+  count: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'redux-app';
 
-  counter: number;
+  counter!: number;
 
   /**
    *
    */
-  constructor() {
-    this.counter = 10;
+  constructor(private _store: Store<AppState>) {}
+  ngOnInit(): void {
+    this._store.select('count').subscribe((count) => {
+      // console.log(count);
+      this.counter = count;
+    });
   }
 
   increment() {
-    this.counter++;
+    this._store.dispatch(actions.increment());
   }
 
   decrement() {
-    this.counter--;
+    this._store.dispatch(actions.decrement());
   }
 }
