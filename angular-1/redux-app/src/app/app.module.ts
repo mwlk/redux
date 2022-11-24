5998b8c6c9da6a18c99505f32a85a6bf88952615
Mirko Wlk <mirkoivowlk@gmail.com>
2022-11-24 00:47:29 -0300

    implement store

--- angular-1/redux-app/src/app/app.module.ts
+++ angular-1/redux-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,18 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { ChildrenComponent } from './components/children/children.component';
-
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './redux/counter.reducer';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent,
-    ChildrenComponent
-  ],
+  declarations: [AppComponent, CounterComponent, ChildrenComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
